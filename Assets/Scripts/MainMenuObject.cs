051bd5952c34f7fbfeab36445be1dab9c6e77e98
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-03-23 17:16:53 +0100

    Graphics

---
+++ Assets/Scripts/MainMenuObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuObject : MonoBehaviour
+{
+	public void PlayBtn ()
+	{
+		SceneManager.LoadScene("MainGame");
+	}
+
+	public void OptionsBtn ()
+	{
+
+	}
+}
