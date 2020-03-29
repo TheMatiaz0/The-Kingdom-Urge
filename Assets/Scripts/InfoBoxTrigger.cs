d65cb6144def7dbe648d1fbd285b509ded44d7b3
TheMatiaz0 <TheMatiaz0@protonmail.com>
2020-03-29 21:59:58 +0200

    Test

---
+++ Assets/Scripts/InfoBoxTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBoxTrigger : MonoBehaviour
+{
+    private GameObject infoBoxObject = null;
+
+    void Start()
+    {
+        // infoBoxObject = GameObject.FindGameObjectWithTag("InfoBox");
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("To działa!");
+    }
+}
